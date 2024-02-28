5d872b8da9a162d1cefcccc3121a693d2e391d82
MartYnX <martynx75@gmail.com>
2024-02-28 12:39:36 +0100

    essai 1

--- CollectInfo.py
+++ CollectInfo.py
@@ -2,6 +2,9 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 import time
 
+def toString(title, href):
+    return "l'article intitule " + title + " a pour lien " + href
+
 browser = webdriver.Chrome()
 browser.get(
     "https://fr.wikipedia.org/w/index.php?search=ordi&title=Sp%C3%A9cial:Recherche&profile=advanced&fulltext=1&ns0=1"
@@ -12,8 +15,6 @@ listTitle = browser.find_elements(By.CLASS_NAME,"mw-search-result-heading")
 def toFormatAndPrint(href, title):
     return print({"href":href, "title":title })
 
-def toString(title, href):
-    return "l'article intitule " + title + " a pour lien " + href
 
 for el in listTitle:
     print(toString(el.find_element(By.CSS_SELECTOR, 'a').get_attribute("title"), el.find_element(By.CSS_SELECTOR, 'a').get_attribute("href")))
